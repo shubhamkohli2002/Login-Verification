f4b62fa05ed5f0895e718c0c4f9860a0e1477be9
shubhamkohli2002 <shubhamk.primotech@gmail.com>
2025-04-22 18:04:37 +0530

    files updated

--- src/test/java/Report/Login.java
+++ src/test/java/Report/Login.java
@@ -1,44 +1,39 @@
 package Report;
 
+import io.qameta.allure.*;
+import io.qameta.allure.testng.AllureTestNg;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Optional;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 import java.util.concurrent.TimeUnit;
 
 import static org.testng.Assert.assertEquals;
 
+@Listeners({AllureTestNg.class})
 public class Login {
     WebDriver driver;
+
     @BeforeTest
-    public void setup(@Optional String browser) throws Exception{
-
-        // Initiate browser driver
+    public void setup(@Optional String browser) {
         driver = new ChromeDriver();
-
-        // adding implicit wait of 12 secs
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-}
+    }
 
     @Test(priority = 1)
+    @Description("Valid login functionality with student credentials")
+    @Severity(SeverityLevel.CRITICAL)
     public void TestCase1() {
-
-     driver.get("https://practicetestautomation.com/practice-test-login/");
-    driver.findElement(By.xpath("//input[@id='username']")).sendKeys("student");
-    driver.findElement(By.xpath("//input[@id='password']")).sendKeys("Password123");
-    driver.findElement(By.xpath("//button[@id='submit']")).click();
+        driver.get("https://practicetestautomation.com/practice-test-login/");
+        driver.findElement(By.id("username")).sendKeys("student");
+        driver.findElement(By.id("password")).sendKeys("Password123");
+        driver.findElement(By.id("submit")).click();
         System.out.println("✅Login verified successfully.");
     }
 
     @AfterTest
     public void teardown() {
-
-        // quitting browser
         driver.quit();
     }
 }
